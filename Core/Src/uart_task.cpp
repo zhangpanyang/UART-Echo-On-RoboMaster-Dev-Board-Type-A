ac0bcec2d2c59f398f8284f3254134daa74c073f
zhangpanyang <azhangpanyang@126.com>
2024-10-03 19:35:35 +0800

    Changed recieve to DMA

--- Core/Src/uart_task.cpp
+++ Core/Src/uart_task.cpp
@@ -8,7 +8,7 @@ uint8_t rx_data[1];
 
 void uartInit()
 {
-	HAL_UART_Receive_IT(&huart8, rx_data, sizeof(rx_data));
+	HAL_UART_Receive_DMA(&huart8, rx_data, sizeof(rx_data));
 	HAL_UART_Transmit(&huart8, (uint8_t*)"init\n", 5, 100);
 }
 
@@ -17,7 +17,7 @@ void HAL_UART_RxCpltCallback(UART_HandleTypeDef *huart)
 	if (huart == &huart8)
 	{
 		HAL_UART_Transmit_IT(&huart8, rx_data, sizeof(rx_data));
-		HAL_UART_Receive_IT(&huart8, rx_data, sizeof(rx_data));
+		HAL_UART_Receive_DMA(&huart8, rx_data, sizeof(rx_data));
 		HAL_GPIO_WritePin(LED_G_GPIO_Port, LED_G_Pin, GPIO_PIN_RESET);
 		__HAL_TIM_SET_COUNTER(&htim6, 0);
 		HAL_TIM_Base_Start_IT(&htim6);
--- MDK-ARM/STM32UARTTest/dma.d
+++ MDK-ARM/STM32UARTTest/dma.d
@@ -1,5 +1,5 @@
-stm32uarttest/main.o: ..\Core\Src\main.c ..\Core\Inc\main.h \
-  ..\Drivers\STM32F4xx_HAL_Driver\Inc\stm32f4xx_hal.h \
+stm32uarttest/dma.o: ..\Core\Src\dma.c ..\Core\Inc\dma.h \
+  ..\Core\Inc\main.h ..\Drivers\STM32F4xx_HAL_Driver\Inc\stm32f4xx_hal.h \
   ..\Core\Inc\stm32f4xx_hal_conf.h \
   ..\Drivers\STM32F4xx_HAL_Driver\Inc\stm32f4xx_hal_rcc.h \
   ..\Drivers\STM32F4xx_HAL_Driver\Inc\stm32f4xx_hal_def.h \
@@ -30,6 +30,4 @@ stm32uarttest/main.o: ..\Core\Src\main.c ..\Core\Inc\main.h \
   ..\Drivers\STM32F4xx_HAL_Driver\Inc\stm32f4xx_hal_pwr_ex.h \
   ..\Drivers\STM32F4xx_HAL_Driver\Inc\stm32f4xx_hal_tim.h \
   ..\Drivers\STM32F4xx_HAL_Driver\Inc\stm32f4xx_hal_tim_ex.h \
-  ..\Drivers\STM32F4xx_HAL_Driver\Inc\stm32f4xx_hal_uart.h \
-  ..\Core\Inc\dma.h ..\Core\Inc\tim.h ..\Core\Inc\usart.h \
-  ..\Core\Inc\gpio.h ..\Core\Src\start.h
+  ..\Drivers\STM32F4xx_HAL_Driver\Inc\stm32f4xx_hal_uart.h
