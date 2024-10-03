acd3a4b465e53273f7295f1522dfa3d803badd75
zhangpanyang <azhangpanyang@126.com>
2024-10-03 20:52:05 +0800

    HAL_UARTEx_RxEventCallback

--- Core/Src/uart_task.cpp
+++ Core/Src/uart_task.cpp
@@ -4,29 +4,31 @@
 
 #include "uart_task.h"
 
-uint8_t rx_data[1];
+uint8_t rx_data[256];
 
 void uartInit()
 {
-	// __HAL_UART_ENABLE_IT(&huart6, UART_IT_IDLE);
+	__HAL_UART_ENABLE_IT(&huart6, UART_IT_IDLE);
 
-	HAL_UART_Receive_DMA(&huart6, rx_data, sizeof(rx_data));
+	HAL_UARTEx_ReceiveToIdle_DMA(&huart6, rx_data, sizeof(rx_data));
+	// HAL_UART_Receive_DMA(&huart6, rx_data, sizeof(rx_data));
 	HAL_UART_Transmit(&huart6, (uint8_t*)"init\n", 5, 100);
 }
 
-// void UART8_IRQHandler(void)
-// {
-// 	if(__HAL_UART_GET_FLAG(&huart6, UART_FLAG_IDLE) != RESET)
-// 	{
-// 		__HAL_UART_CLEAR_FLAG(&huart6, UART_FLAG_IDLE);
-// 		HAL_UART_Transmit_IT(&huart6, rx_data, sizeof(rx_data));
-// 		HAL_UART_Receive_DMA(&huart6, rx_data, sizeof(rx_data));
-//
-// 		HAL_GPIO_WritePin(LED_G_GPIO_Port, LED_G_Pin, GPIO_PIN_RESET);
-// 		__HAL_TIM_SET_COUNTER(&htim6, 0);
-// 		HAL_TIM_Base_Start_IT(&htim6);
-// 	}
-// }
+void HAL_UARTEx_RxEventCallback(UART_HandleTypeDef *huart, uint16_t Size)
+{
+	HAL_GPIO_WritePin(LED_R_GPIO_Port, LED_R_Pin, GPIO_PIN_RESET);
+	if(__HAL_UART_GET_FLAG(&huart6, UART_FLAG_IDLE) != RESET)
+	{
+		__HAL_UART_CLEAR_FLAG(&huart6, UART_FLAG_IDLE);
+		HAL_UART_Transmit_IT(&huart6, rx_data, sizeof(rx_data));
+		HAL_UART_Receive_DMA(&huart6, rx_data, sizeof(rx_data));
+
+		HAL_GPIO_WritePin(LED_G_GPIO_Port, LED_G_Pin, GPIO_PIN_RESET);
+		__HAL_TIM_SET_COUNTER(&htim6, 0);
+		HAL_TIM_Base_Start_IT(&htim6);
+	}
+}
 
 void HAL_UART_RxCpltCallback(UART_HandleTypeDef *huart)
 {
